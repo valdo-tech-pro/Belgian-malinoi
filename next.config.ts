5fe5e11e755da3c4c3e5a4e75a333b93d52e86a0
valdo-tech-pro <144343046+valdo-tech-pro@users.noreply.github.com>
2026-08-05 22:16:41 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**", // Allows all HTTPS images. You can restrict this later if needed.
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY", // Prevents your site from being embedded in iframes (clickjacking protection)
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff", // Prevents MIME type sniffing
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin", // Controls how much referrer info is sent
-          },y
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig; 
+export default nextConfig;
